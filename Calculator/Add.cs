d5d63ceb815e9dac0b4b5b91a35a232c7ab86dc9
Pratham Jain <Pratham.J_INT1598@accolite.com>
2024-04-10 16:36:29 +0530

    first

---
+++ Calculator/Add.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Add
+    {
+        public static double Addition(double a, double b)
+        {
+            return a + b; 
+        }public static int Addition(int a, int b)
+        {
+            return a + b; 
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Calculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            double number1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            double number2 = double.Parse(Console.ReadLine());
+
+            double sum = Add.Addition(number1, number2);
+            Console.WriteLine("The sum is " + sum);
+
+
+
+        }
+    }
+}
